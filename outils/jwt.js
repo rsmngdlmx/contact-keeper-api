261d330ebfab0503ad6c6ae01a8066c9fdb4fbcd
Ricardo Samuel Mendoza Núñez <rsmn.development@gmail.com>
2020-10-20 03:01:18 -0500

    Fix error with jwt not returning token

--- outils/jwt.js
+++ outils/jwt.js
@@ -5,10 +5,7 @@ const createToken = userId => {
   const payload = { user: { id: userId } };
   const options = { expiresIn: 360000 };
 
-  return jwt.sign(payload, config.get('jwtSecret'), options, (err, token) => {
-    if (err) throw err;
-    return token;
-  });
+  return jwt.sign(payload, config.get('jwtSecret'), options);
 };
 
 module.exports = { createToken };
